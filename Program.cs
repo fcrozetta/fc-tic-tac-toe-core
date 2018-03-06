1a9b294042584b9cab3fff1d0aab45564f1f4f1a
Fernando Crozetta <fernando@czetta.com>
2018-03-06 18:30:20 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using tui_netcore;
+using System.Collections.Generic;
+
+namespace fc_tic_tac_toe_core
+{
+    class Program
+    {
+        public List<List<string>> Board { get; set; }
+
+        public static void Quit(int errNum=0){
+            Console.CursorVisible = true;
+            Environment.Exit(errNum);
+        }
+
+        
+
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            Tui t = new Tui(100, 50)
+            {
+                Title = "Tic Tac Toe",
+                Body  = "Welcome to Tic Tac Toe! \n This is a game for 2 Players. \n This Game was meant to be played only one time. \n Enjoy"
+            };
+            t.DrawOk();
+
+            
+
+        }
+    }
+}
